41f6bb18f80693a4c010edc562e8a0eaf0c74288
Axzyte <101258813+4rw1n@users.noreply.github.com>
2023-12-02 21:32:53 +0545

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void banner();
+int main(){
+        do{
+           system("cls");
+            banner();
+        }
+        while(true);
+        return 0;
+}
+void banner(){
+        cout <<"__________________________________________"<<endl;
+        cout <<"               TUI-BANKING SYSTEM          "<<endl;
+        cout <<"__________________________________________"<<endl;
+}
